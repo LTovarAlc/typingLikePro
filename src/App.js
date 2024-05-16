e550e12eef9f78d5f00623e64bed2658fe7dbbd5
Luis Tovar <ltovaralc@gmail.com>
2024-05-15 19:41:21 -0600

    add message at virualScreen

--- src/App.js
+++ src/App.js
@@ -1,6 +1,4 @@
 import "./App.css";
-import MechanographicLayout from "./components/mechanographic-layout/mechanographic-layout";
-import { useState } from "react";
 import { BrowserRouter as Router, Routes, Route} from "react-router-dom";
 import KeyboardPage from "./pages/keyboardPage/keyboardPage";
 
--- src/components/screen/screen.jsx
+++ src/components/screen/screen.jsx
@@ -1,9 +1,26 @@
 import "./screen.css"
 
 const VirtualScreen = ({text}) => {
+
+    // paragraph to write
+    const paragraph = [
+        "Birds chirped in the morning, sun rising over hills, painting skies with hues of gold an orange, picturesque dawn.",
+        "Waves whisperes secrets to the shore, sand absorbing storis of the vast, mysterious ocean, timeless tales.",
+        'City ligths twinkled like stars. streets alive with the rhythm of footstep, dreams pulsting in every corner.',
+        'Leaves rustled in the breeze, trees swaying in a dance of nature, harmonious melodies sung by the wind.',
+        "Raindrops tapped on windows, a symphony of nature's percussion, composing melodies of tranquility.",
+        "Mountains stood tall and majestic, guardians of ancient lands, their peaks touching the heavens.",
+        "Flames flickered in the fireplace, casting warm shadows, a sanctuary from the cold of winter nights.",
+        "Flowers bloomed in vibrant colors, petals unfurling like whispers of love, beauty unfolding with each sunrise.",
+        "Snowflakes danced in the air, swirling and twirling in a graceful ballet, a winter wonderland's enchanting charm.",
+        "Stars adorned the night sky, guiding lost souls, constellations weaving tales of the cosmos, infinite possibilities."
+    ]
+
+    const initialMessge = paragraph[0];
+
     return( 
     <div className="screen">
-        {text}
+        <p>{initialMessge}</p>    
     </div>
     )
 }
