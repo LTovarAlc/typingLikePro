c2f61a9930271262e5487058421e9552301ced6b
Luis Tovar <ltovaralc@gmail.com>
2024-05-20 18:48:11 -0600

    add background

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
 import { BrowserRouter as Router, Routes, Route} from "react-router-dom";
 import KeyboardPage from "./pages/keyboardPage/keyboardPage";
+import { Background } from "./components/background/background";
 
 function App() {
   return (
     <Router>
+      <Background/>
       <Routes>
         <Route path="/VirtualKeyboard" element={<KeyboardPage/>}/>
       </Routes>
---
+++ src/components/background/background.jsx
@@ -0,0 +1,7 @@
+import "./background.css"
+
+export const Background = () => {
+    return(
+        <div class="container"></div>
+    )
+}
