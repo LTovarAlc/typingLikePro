2dfa35002e84a8c5262b508923a24cade9e0d1c2
Luis Tovar <ltovaralc@gmail.com>
2024-06-11 18:44:14 -0600

    add header

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -3,15 +3,7 @@ import "./header.css"
 const Header = () => {
     return(
         <header className="header">
-            <div className="logo-container">
-                <h1>TLICK</h1>
-            </div>
-            <nav className="menu">
-                <ul>
-                    <li>Instructions</li>
-                    <li>Try minigame</li>
-                </ul>
-            </nav>
+                <h1 className="page-name">TapJoy</h1>
         </header>
     )
 }
